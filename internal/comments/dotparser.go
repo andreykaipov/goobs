e6235cb1f7831915c1b71d0f6979cbf6b4a1a144
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2021-04-25 20:32:15 -0400

    add slice support for dot parser crap

--- internal/comments/dotparser.go
+++ internal/comments/dotparser.go
@@ -9,30 +9,6 @@ import (
 	"github.com/dave/jennifer/jen"
 )
 
-// a.b.c = int
-// a.*.c = int
-// a.b.*.c = int
-//
-// a = { b = { c = 1 } }
-// a = [ { c = 1 }, { c = 2 } ]
-// a = { b = [ { c = 1 }, { c = 2 } ] }
-//
-// a struct {
-//   b struct {
-//   	c int
-//   }
-// }
-// a []struct {
-//   c int
-// }
-// a struct {
-//   b []struct {
-//     c int
-//   }
-// }
-
-// e.g. a.b.c = int
-
 func parseJenKeysAsMap(lines map[string]jen.Code) (map[string]interface{}, error) {
 	// e.g. keeps a reference from a.b to the corresponding struct
 
@@ -96,11 +72,21 @@ func parseJenKeysAsStruct(name string, lines map[string]jen.Code) (*jen.Statemen
 		return nil, err
 	}
 
+	// returns whether to skip the current group (slice support)
 	var f func(data interface{}, g *jen.Group, parent string)
 
 	f = func(data interface{}, g *jen.Group, parent string) {
 		switch t := data.(type) {
 		case map[string]interface{}:
+			// if there's an *, we should redo the recursion with a slice
+			for _, k := range sortedKeys(t) {
+				v := t[k]
+				if k == "*" {
+					f(v, g, parent+"[]")
+					return
+				}
+			}
+
 			g.Id(parent).StructFunc(func(subg *jen.Group) {
 				for _, k := range sortedKeys(t) {
 					v := t[k]
@@ -113,7 +99,6 @@ func parseJenKeysAsStruct(name string, lines map[string]jen.Code) (*jen.Statemen
 			panic("unhandled case idk")
 		}
 	}
-	fmt.Printf("%#v\n", m)
 
 	return jen.Type().CustomFunc(jen.Options{}, func(g *jen.Group) {
 		f(m, g, name)
@@ -143,38 +128,3 @@ func sortedKeys(m interface{}) []string {
 	sort.Strings(sorted)
 	return sorted
 }
-
-/*
-func printJSON(data interface{}, indent int) {
-	switch t := data.(type) {
-	case map[string]interface{}:
-		fmt.Println("{")
-		i := 0
-		for k, v := range t {
-			fmt.Printf("%*.s%q: ", indent+2, " ", k)
-			printJSON(v, indent+2)
-			if i < len(t)-2 {
-				fmt.Printf(",\n")
-			}
-			i++
-		}
-		fmt.Printf("\n%*.s}", indent, " ")
-	case *VariableArray:
-		fmt.Println("[")
-		for i, v := range t.Iterator() {
-			fmt.Printf("%*.s", indent+2, " ")
-			printJSON(v, indent+2)
-			if i < t.Len()-1 {
-				fmt.Printf(",\n")
-			}
-		}
-		fmt.Printf("\n%*.s]", indent, " ")
-	case nil:
-		fmt.Print("null")
-	case string:
-		fmt.Printf("%q", data)
-	default:
-		fmt.Print(data)
-	}
-}
-*/
---
+++ internal/comments/dotparser_test.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/dave/jennifer/jen"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_parseKeysAsJenStruct_basic(t *testing.T) {
+	statement, err := parseJenKeysAsStruct("StartStreamingRequest", map[string]jen.Code{
+		"Stream":                   jen.Map(jen.String()).Interface(),
+		"Stream.Type":              jen.Int(),
+		"Stream.Metadata":          jen.Map(jen.String()).Interface(),
+		"Stream.Settings":          jen.Map(jen.String()).Interface(),
+		"Stream.Settings.Server":   jen.String(),
+		"Stream.Settings.Key":      jen.String(),
+		"Stream.Settings.UseAuth":  jen.Bool(),
+		"Stream.Settings.Username": jen.String(),
+		"Stream.Settings.Password": jen.String(),
+	})
+
+	buf := new(bytes.Buffer)
+	statement.Render(buf)
+	actual := buf.String()
+	// fmt.Println(actual)
+
+	expected := `type StartStreamingRequest struct {
+	Stream struct {
+		Metadata map[string]interface{}
+		Settings struct {
+			Key      string
+			Password string
+			Server   string
+			UseAuth  bool
+			Username string
+		}
+		Type int
+	}
+}`
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func Test_parseKeysAsJenStruct_slices(t *testing.T) {
+	statement, err := parseJenKeysAsStruct("GetSourcesListRequest", map[string]jen.Code{
+		"Sources":          jen.Index().Map(jen.String()).Interface(),
+		"Sources.*.Name":   jen.String(),
+		"Sources.*.TypeId": jen.String(),
+		"Sources.*.Type":   jen.String(),
+	})
+
+	buf := new(bytes.Buffer)
+	statement.Render(buf)
+	actual := buf.String()
+	// fmt.Println(actual)
+
+	expected := `type GetSourcesListRequest struct {
+	Sources []struct {
+		Name   string
+		Type   string
+		TypeId string
+	}
+}`
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+
+}
+
+func Test_parseKeysAsJenStruct_slices_nested(t *testing.T) {
+	statement, err := parseJenKeysAsStruct("GetSourcesTypesListResponse", map[string]jen.Code{
+		"Ids":                         jen.Index().Map(jen.String()).Interface(),
+		"Ids.*.TypeId":                jen.String(),
+		"Ids.*.DisplayName":           jen.String(),
+		"Ids.*.Type":                  jen.String(),
+		"Ids.*.DefaultSettings":       jen.Map(jen.String()).Interface(),
+		"Ids.*.Caps":                  jen.Map(jen.String()).Interface(),
+		"Ids.*.Caps.IsAsync":          jen.Bool(),
+		"Ids.*.Caps.HasVideo":         jen.Bool(),
+		"Ids.*.Caps.HasAudio":         jen.Bool(),
+		"Ids.*.Caps.CanInteract":      jen.Bool(),
+		"Ids.*.Caps.IsComposite":      jen.Bool(),
+		"Ids.*.Caps.DoNotDuplicate":   jen.Bool(),
+		"Ids.*.Caps.DoNotSelfMonitor": jen.Bool(),
+		"Ids.*.Caps.Extra.*.Wow":      jen.Bool(),
+		"Ids.*.Caps.Extra.*.Poopy":    jen.Bool(),
+	})
+
+	buf := new(bytes.Buffer)
+	statement.Render(buf)
+	actual := buf.String()
+	// fmt.Println(actual)
+
+	expected := `type GetSourcesTypesListResponse struct {
+	Ids []struct {
+		Caps struct {
+			CanInteract      bool
+			DoNotDuplicate   bool
+			DoNotSelfMonitor bool
+			Extra            []struct {
+				Poopy bool
+				Wow   bool
+			}
+			HasAudio    bool
+			HasVideo    bool
+			IsAsync     bool
+			IsComposite bool
+		}
+		DefaultSettings map[string]interface{}
+		DisplayName     string
+		Type            string
+		TypeId          string
+	}
+}`
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func Test_parseKeysAsJenStruct_slices_legacy(t *testing.T) {
+	statement, err := parseJenKeysAsStruct("ReorderSceneItemsRequest_Legacy", map[string]jen.Code{
+		"Items":        jen.Index().Map(jen.String()).Interface(),
+		"Items[].Id":   jen.Int(),
+		"Items[].Name": jen.String(),
+	})
+
+	buf := new(bytes.Buffer)
+	statement.Render(buf)
+	actual := buf.String()
+	// fmt.Println(actual)
+
+	expected := `type ReorderSceneItemsRequest_Legacy struct {
+	Items []struct {
+		Id   int
+		Name string
+	}
+}`
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}

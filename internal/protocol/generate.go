aa3d5d1ce94bc639695119a7424d0dd1d3111a64
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2022-07-24 22:34:24 -0400

    protocol: generate requests

--- internal/protocol/generate.go
+++ internal/protocol/generate.go
@@ -15,6 +15,171 @@ func lower(s string) string {
 	return strings.ToLower(s)
 }
 
+func generateRequests(requests []*Request) {
+	topClientFields := []Code{}
+	topClientSetters := []Code{}
+
+	// aggregate requests by category
+	categories := map[string][]*Request{}
+	for _, request := range requests {
+		categories[request.Category] = append(categories[request.Category], request)
+	}
+
+	for category, requests := range categories {
+		categorySnake := strings.ReplaceAll(category, " ", "_")
+		categoryPascal := strings.ReplaceAll(strings.Title(category), " ", "")
+		categoryClaustrophic := strings.ReplaceAll(category, " ", "")
+
+		// For the top-level client
+		qualifier := goobs + "/api/requests/" + categorySnake
+		topClientFields = append(topClientFields, Id(categoryPascal).Op("*").Qual(qualifier, "Client"))
+		topClientSetters = append(
+			topClientSetters, Id("c").Dot(categoryPascal).Op("=").Op("&").Qual(qualifier, "Client").Values(
+				Id("Client").Op(":").Id("c").Dot("Client"),
+			),
+		)
+
+		// Generate the category-level client
+		client := NewFile(categoryClaustrophic)
+		client.HeaderComment("This file has been automatically generated. Don't edit it.")
+		client.Commentf("Client represents a client for '%s' requests.", category)
+		client.Add(
+			Type().Id("Client").Struct(
+				Op("*").Qual(goobs+"/api/requests", "Client"),
+			),
+		)
+
+		// Write the category-level client
+		dir := fmt.Sprintf("%s/api/requests/%s", root, categorySnake)
+		if err := os.MkdirAll(dir, 0777); err != nil {
+			panic(err)
+		}
+		if err := client.Save(fmt.Sprintf("%s/zz_generated.client.go", dir)); err != nil {
+			panic(err)
+		}
+
+		// Generate the requests for the category
+		for _, request := range requests {
+			name := request.RequestType
+
+			if request.Deprecated {
+				// fmt.Fprintf(os.Stderr, "Request %q is deprecated\n", request.Name)
+				continue
+			}
+
+			s, err := generateRequest(request)
+			if err != nil {
+				panic(err)
+			}
+
+			f := NewFile(categoryClaustrophic)
+			f.HeaderComment("This file has been automatically generated. Don't edit it.")
+			f.Add(s)
+			fName := strings.ToLower(name)
+			if err := f.Save(fmt.Sprintf("%s/xx_generated.%s.go", dir, fName)); err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	// Write utils for the top-level client
+	f := NewFile("goobs")
+	f.HeaderComment("This file has been automatically generated. Don't edit it.")
+	f.Add(Type().Id("subclients").Struct(topClientFields...))
+	f.Add(Func().Id("setClients").Params(Id("c").Op("*").Id("Client")).Block(topClientSetters...))
+
+	if err := f.Save(fmt.Sprintf("%s/zz_generated.client.go", root)); err != nil {
+		panic(err)
+	}
+}
+
+func generateRequest(request *Request) (s *Statement, err error) {
+	category := request.Category
+	name := request.RequestType
+
+	fmt.Printf("Request %s %s\n", category, name)
+
+	var structName string
+	s = Line()
+
+	// Params
+	structName = name + "Params"
+	s.Commentf("%s represents the params body for the %q request.\n%s\n", structName, name, request.Description).Line()
+
+	reqf := &RequestField{}
+	reqf.ValueName = "ParamsBasic"
+	reqf.ValueType = "~requests~" // internal type
+	reqf.ValueDescription = ""
+	request.RequestFields = append(request.RequestFields, reqf)
+
+	if err = generateStructFromParams("request", s, structName, request.RequestFields); err != nil {
+		return nil, fmt.Errorf("Failed parsing 'Params' for request %q in category %q", name, category)
+	}
+
+	s.Add(
+		Commentf("GetSelfName just returns %q.", name).Line(),
+		Func().Params(Id("o").Op("*").Id(structName)).Id("GetSelfName").Params().String().Block(
+			Return(Lit(name)),
+		).Line(),
+	)
+
+	// Returns
+	structName = name + "Response"
+	s.Commentf("%s represents the response body for the %q request.\n%s\n", structName, name, request.Description).Line()
+
+	respf := &ResponseField{}
+	respf.ValueName = "ResponseBasic"
+	respf.ValueType = "~requests~" // internal type
+	respf.ValueDescription = ""
+	request.ResponseFields = append(request.ResponseFields, respf)
+
+	if err = generateStructFromParams("response", s, structName, request.ResponseFields); err != nil {
+		return nil, fmt.Errorf("Failed parsing 'Returns' for request %q in category %q", name, category)
+	}
+
+	// generate the request function
+
+	hasRequiredArgs := len(request.RequestFields) > 1
+
+	s.Commentf("%s sends the corresponding request to the connected OBS WebSockets server.", name).Do(func(z *Statement) {
+		if hasRequiredArgs {
+			return
+		}
+		z.Id("Note the variadic arguments as this request doesn't require any parameters.")
+	})
+	s.Line()
+	s.Func().Params(Id("c").Op("*").Id("Client")).Id(name).ParamsFunc(func(g *Group) {
+		if hasRequiredArgs {
+			g.Id("params").Op("*").Id(name + "Params")
+		} else {
+			g.Id("paramss").Op("...").Op("*").Id(name + "Params")
+		}
+	}).Params(Op("*").Id(name+"Response"), Error()).Block(
+		Do(func(z *Statement) {
+			if hasRequiredArgs {
+				return
+			}
+			z.If(Len(Id("paramss")).Op("==").Lit(0)).Block(
+				Id("paramss").Op("=").Index().Op("*").Id(name + "Params").Values(Values()),
+			)
+			z.Line()
+			z.Id("params").Op(":=").Id("paramss").Index(Lit(0))
+		}),
+		Id("data").Op(":=").Op("&").Id(name+"Response").Values(),
+		If(
+			Id("err").Op(":=").Id("c").Dot("SendRequest").Call(
+				Id("params"), Id("data"),
+			),
+			Id("err").Op("!=").Nil(),
+		).Block(
+			Return().List(Nil(), Id("err")),
+		),
+		Return().List(Id("data"), Nil()),
+	)
+
+	return s, nil
+}
+
 func generateEvents(events []*Event) {
 	// Unlike requests, I don't want to write each event into a package
 	// representing the category, since I want the syntax to read like
@@ -27,8 +192,7 @@ func generateEvents(events []*Event) {
 
 	for _, e := range events {
 		category := snake(e.Category)
-
-		fmt.Printf("%-12s %s\n", e.Category, e.EventType)
+		name := e.EventType
 
 		s, err := generateEvent(e)
 		if err != nil {
@@ -38,7 +202,7 @@ func generateEvents(events []*Event) {
 		f := NewFile("events")
 		f.HeaderComment("This file has been automatically generated. Don't edit it.")
 		f.Add(s)
-		fName := lower(e.EventType)
+		fName := lower(name)
 		if err := f.Save(fmt.Sprintf("%s/xx_generated.%s.%s.go", dir, category, fName)); err != nil {
 			panic(err)
 		}
@@ -60,6 +224,26 @@ func generateEvents(events []*Event) {
 	if err := f.Save(fmt.Sprintf("%s/zz_generated.events.go", dir)); err != nil {
 		panic(err)
 	}
+}
+
+func generateEvent(event *Event) (s *Statement, err error) {
+	fmt.Printf("Event %s %s\n", event.Category, event.EventType)
+
+	s = Line()
+	s.Commentf("%s represents the event body for the %q event.\nSince v%s.", event.EventType, event.EventType, event.InitialVersion).Line()
+
+	//	df := &DataField{}
+	//	df.ValueName = "EventBasic"
+	//	df.ValueType = "~events~" // internal type
+	//	df.ValueDescription = ""
+	//
+	//	event.DataFields = append(event.DataFields, df)
+
+	if err = generateStructFromParams("event", s, event.EventType, event.DataFields); err != nil {
+		return nil, fmt.Errorf("Failed generating event %q in category %q", event.EventType, event.Category)
+	}
+
+	return s, nil
 }
 
 func generateEventSubscriptions(enums []*Enum) {
@@ -88,7 +272,7 @@ func generateEventSubscriptions(enums []*Enum) {
 			id := subscription.EnumIdentifier
 			description := subscription.Description
 
-			fmt.Printf("%-12s %s\n", "Subscription", id)
+			fmt.Printf("EventSubscription %s\n", id)
 
 			f.Add(
 				Comment(description),
@@ -101,18 +285,6 @@ func generateEventSubscriptions(enums []*Enum) {
 			panic(err)
 		}
 	}
-}
-
-func generateEvent(event *Event) (s *Statement, err error) {
-	s = Line()
-	s.Commentf("%s represents the event body for the %q event.\nSince v%s.", event.EventType, event.EventType, event.InitialVersion).Line()
-
-	//event.Returns = append(event.Returns, &Param{Name: "EventBasic", Type: "~events~"}) // internal type
-	if err = generateStructFromParams("event", s, event.EventType, event.DataFields); err != nil {
-		return nil, fmt.Errorf("Failed generating event %q in category %q", event.EventType, event.Category)
-	}
-
-	return s, nil
 }
 
 func generateStructFromParams(origin string, s *Statement, name string, fields interface{}) error {
@@ -144,6 +316,7 @@ func generateStructFromParams(origin string, s *Statement, name string, fields i
 		fvn := f.GetValueName()
 		fvt := f.GetValueType()
 		fvd := f.GetValueDescription()
+		embedded := false
 
 		//fieldName, err := sanitizeText(field.Name)
 		//if err != nil {
@@ -164,6 +337,14 @@ func generateStructFromParams(origin string, s *Statement, name string, fields i
 			fieldType = Interface()
 		case "Boolean":
 			fieldType = Bool()
+		case "Any":
+			fieldType = Interface()
+		case "~requests~":
+			fieldType = Qual(goobs+"/api/requests", fvn)
+			embedded = true
+		case "~events~":
+			fieldType = Id(fvn)
+			embedded = true
 		default:
 			panic(fmt.Errorf("in struct %q, %q is of weird type %q", name, fvn, fvt))
 		}
@@ -172,12 +353,12 @@ func generateStructFromParams(origin string, s *Statement, name string, fields i
 			Type:      fieldType,
 			Comment:   fvd,
 			NoJSONTag: false,
-			Embedded:  false,
+			Embedded:  embedded,
 			OmitEmpty: true,
 		}
 	}
 
-	fmt.Printf("%#v\n", keysInfo)
+	//fmt.Printf("%#v\n", keysInfo)
 	statement, err := parseJenKeysAsStruct(name, keysInfo)
 	if err != nil {
 		return fmt.Errorf("Failed parsing dotted key: %s", err)

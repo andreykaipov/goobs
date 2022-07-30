a864dc03cef7a9818b4d8bfcb7af58ba9e648ae0
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2022-07-30 17:42:14 -0400

    api: handle request responses!

--- api/requests/client.go
+++ api/requests/client.go
@@ -1,12 +1,13 @@
 package requests
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"time"
 
+	"github.com/andreykaipov/goobs/api/opcodes"
 	"github.com/andreykaipov/goobs/apiv5/events"
+	"github.com/buger/jsonparser"
 	"github.com/gorilla/websocket"
 	uuid "github.com/nu7hatch/gouuid"
 )
@@ -70,31 +71,21 @@ func (c *Client) Disconnect() error {
 // interestingly, events appear to be broadcast to every client, maybe because
 // they have no associated message ID?
 func (c *Client) SendRequest(params Params, response Response) error {
-	params.SetRequestType(params.GetSelfName())
-
-	if params.GetRequestType() == "" {
-		return fmt.Errorf("Params %T has an empty RequestType", params)
-	}
-
-	if params.GetMessageID() == "" {
-		id, err := uuid.NewV4()
-		if err != nil {
-			return err
-		}
-
-		params.SetMessageID(id.String())
-	}
-
-	paramsMarshalled, err := json.Marshal(params)
+	id, err := uuid.NewV4()
 	if err != nil {
-		return fmt.Errorf("Couldn't marshal params: %s", err)
-	}
-
-	c.Log.Printf("Sent request: %s", paramsMarshalled)
-
-	if err := c.Conn.WriteMessage(websocket.TextMessage, paramsMarshalled); err != nil {
 		return err
 	}
+
+	request := opcodes.Wrap(&opcodes.Request{
+		RequestType: params.GetSelfName(),
+		RequestID:   id.String(),
+		RequestData: params,
+	})
+	if err := c.Conn.WriteMessage(websocket.TextMessage, request); err != nil {
+		panic(fmt.Errorf("Couldn't write message: %w", err))
+	}
+
+	c.Log.Printf("Sent request: %s", request)
 
 	var msg json.RawMessage
 	select {
@@ -103,34 +94,48 @@ func (c *Client) SendRequest(params Params, response Response) error {
 		return fmt.Errorf("Timed out receiving response from server for request %q", params.GetRequestType())
 	}
 
-	// OBS websocket seems to return already pretty-printed JSON in its
-	// responses, so we compact the response to print it on just one line.
-	// It's pretty wasteful if debug logging is not enabled since it'll just
-	// be discarded, but eh it's not too big of a deal...
-	buffer := &bytes.Buffer{}
-	_ = json.Compact(buffer, msg)
-	c.Log.Printf("Raw response: %s", buffer)
+	// TODO parse requestId and requestStatus as common response params
 
-	if err := json.Unmarshal(msg, response); err != nil {
+	c.Log.Printf("Raw response: %s", msg)
+
+	responseData, _, _, err := jsonparser.Get(msg, "responseData")
+	if err != nil {
+		return fmt.Errorf("Couldn't find 'responseData' key from response: %s", err)
+	}
+
+	if err := json.Unmarshal(responseData, response); err != nil {
 		return fmt.Errorf("Couldn't unmarshal message into an unknown event: %s", err)
 	}
 
-	// gorilla/websocket would panic before this could happen in
-	// a concurrent context, and idk how this could ever happen otherwise
-	if response.GetMessageID() != params.GetMessageID() {
+	//
+	responseCode, err := jsonparser.GetInt(msg, "requestStatus", "code")
+	if err != nil {
+		return fmt.Errorf("Couldn't find 'requestStatus.code' key from response: %s", err)
+	}
+	responseComment, _ := jsonparser.GetString(msg, "requestStatus", "comment")
+	responseMessageID, err := jsonparser.GetString(msg, "requestId")
+	if err != nil {
+		return fmt.Errorf("Couldn't find 'requestId' key from response: %s", err)
+	}
+
+	// request ID on response should be a mirror of what the client sent, so
+	// this is good to check. however, I'm pretty sure a mismatch like this
+	// could only happen in a concurrent context... and I'm pretty sure
+	// gorilla/websocket would panic before then, so... 🤷
+	if responseMessageID != id.String() {
 		return fmt.Errorf(
 			"Sent request %s, with message ID %s, but next response in channel has message ID %s",
 			params.GetSelfName(),
-			params.GetMessageID(),
-			response.GetMessageID(),
+			id,
+			responseMessageID,
 		)
 	}
 
-	if response.GetStatus() == "error" {
+	if responseCode != 100 {
 		return fmt.Errorf(
 			"Got error from OBS executing request %q: %s",
 			params.GetRequestType(),
-			response.GetError(),
+			responseComment,
 		)
 	}
 

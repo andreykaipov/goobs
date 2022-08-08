0a96dca18c11b1a6549a90ec67a00f0adefc0b92
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2022-08-08 00:00:52 -0400

    get ci workflow working

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ help:
 	@echo "Please specify a task:"
 	@awk -F: '/^[^\t#$$]+:[^=]+?$$/ {print "-",$$1}' Makefile
 
+.PHONY: test
 test: test.unit test.functional
-
 test.unit:
 	cd internal; go test -v -count 1 ./...
 test.functional:
@@ -17,6 +17,8 @@ generate:
 format:
 	go install github.com/segmentio/golines@latest
 	golines --shorten-comments --max-len=120 --reformat-tags --write-output *.go api
+	go install golang.org/x/tools/cmd/goimports@latest
+	go list -f '{{.Dir}}' ./... | xargs goimports -w
 
 clean:
 	find . -regextype awk -regex "./cover.+(out|html)" | xargs rm -f
--- test.sh
+++ test.sh
@@ -5,20 +5,20 @@ cleanup() { docker stop obs-record obs-stream; }
 setup() {
         echo "Setting up OBS instances for functional tests..."
 
-        obs="$(docker container inspect -f '{{.State.Status}}' obs)"
+        obs="$(docker container inspect -f '{{.State.Status}}' obs || true)"
 
         if [ "$obs" = running ]; then
                 echo "Main OBS container is already running"
         else
-                docker run --rm --detach --name obs -p 4444:1234 "$(docker build -q docker)"
+                docker run --rm --detach --name obs -p 4444:1234 ghcr.io/andreykaipov/goobs
         fi
 
         # record and stream categories aren't totally idempotent so we need
         # a clean OBS docker instance each time
 
         echo "Spinning up OBS instances for 'record' and 'stream' tests"
-        docker run --rm --detach --name obs-record -p 4445:1234 "$(docker build -q docker)"
-        docker run --rm --detach --name obs-stream -p 4446:1234 "$(docker build -q docker)"
+        docker run --rm --detach --name obs-record -p 4445:1234 ghcr.io/andreykaipov/goobs
+        docker run --rm --detach --name obs-stream -p 4446:1234 ghcr.io/andreykaipov/goobs
 
         covermode=count
         echo "mode: $covermode" >coverall.out
@@ -31,6 +31,7 @@ gotest() {
 }
 
 main() {
+        set -eu
         trap cleanup EXIT
         setup
         export OBS_PORT

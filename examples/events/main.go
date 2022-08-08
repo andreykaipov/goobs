cf07925957bc11ae672a32d88c94c51f3a1ac8fd
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2022-08-07 23:57:49 -0400

    update examples

--- examples/events/main.go
+++ examples/events/main.go
@@ -6,34 +6,27 @@ import (
 	"os"
 
 	"github.com/andreykaipov/goobs"
+	"github.com/andreykaipov/goobs/api/events"
 )
 
-type logger struct{}
-
-func (l *logger) Printf(f string, v ...interface{}) {
-	fmt.Fprintf(os.Stderr, "\033[1;33m"+f+"\033[0m\n", v...)
-}
-
 func main() {
-	client, err := goobs.New("localhost:4444", goobs.WithPassword("goodpassword"))
-	if err != nil {
-		panic(err)
-	}
+	client, _ := goobs.New("localhost:4444", goobs.WithPassword("goodpassword"))
 	defer client.Disconnect()
 
 	version, _ := client.General.GetVersion()
 	fmt.Printf("OBS Studio version: %s\n", version.ObsVersion)
 	fmt.Printf("Websocket server version: %s\n", version.ObsWebSocketVersion)
 
-	// This event loop is in the foreground now. If you mess around in OBS,
-	// you'll see different events popping up!
-
 	for event := range client.IncomingEvents {
 		switch e := event.(type) {
+		case *events.ExitStarted:
+			log.Printf("Server closing; gracefully shutting down")
+			log.Printf("Bye")
+			os.Exit(0)
 		case error:
-			log.Printf("Got an error: %s", e)
+			log.Printf("Error: %s", e)
 		default:
-			log.Printf("Unhandled event: %[1]T: %#[1]v", e)
+			log.Printf("Unhandled event: %#v", e)
 		}
 	}
 }

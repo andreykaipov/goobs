ce9a2bee90de1d2a717d6c49b0a7c58d99c356b1
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2022-08-07 22:04:07 -0400

    examples

---
+++ examples/direct/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:    4096,
+	WriteBufferSize:   4096,
+	EnableCompression: true,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func test(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade:", err)
+		return
+	}
+	defer conn.Close()
+}
+
+func main() {
+	http.HandleFunc("/test", test)
+	log.Fatal(http.ListenAndServe(":9000", nil))
+}
--- examples/sources/main.go
+++ examples/sources/main.go
@@ -4,61 +4,81 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
-	"os"
 	"time"
 
 	"github.com/andreykaipov/goobs"
 	"github.com/andreykaipov/goobs/api/events"
-	"github.com/andreykaipov/goobs/api/requests/sources"
+	"github.com/andreykaipov/goobs/api/requests/inputs"
 )
 
 func main() {
 	client, err := goobs.New(
-		os.Getenv("WSL_HOST")+":4444",
-		goobs.WithPassword("hello"),                   // optional
-		goobs.WithDebug(os.Getenv("OBS_DEBUG") != ""), // optional
+		"localhost:4444",
+		goobs.WithPassword("goodpassword"), // optional
 	)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+	fmt.Printf("%#v\n", client)
 
 	go func() {
 		for event := range client.IncomingEvents {
 			switch e := event.(type) {
-			case *events.SourceVolumeChanged:
-				fmt.Printf("Volume changed for %-25q: %f\n", e.SourceName, e.Volume)
+			case *events.ExitStarted:
+				log.Printf("Server closing; gracefully shutting down")
+				log.Printf("Bye")
+				//os.Exit(0)
+			case error:
+				log.Printf("Error: %s", e)
 			default:
-				log.Printf("Unhandled event: %#v", e.GetUpdateType())
+				log.Printf("Unhandled event: %#v", e)
 			}
 		}
 	}()
 
-	fmt.Println("Setting random volumes for each source...")
+	defer client.Disconnect()
 
-	rand.Seed(time.Now().UnixNano())
-	list, _ := client.Sources.GetSourcesList()
+	getStatsResp, err := client.General.GetStats()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	for _, v := range list.Sources {
-		if _, err := client.Sources.SetVolume(&sources.SetVolumeParams{
-			Source: v.Name,
-			Volume: rand.Float64(),
+	fmt.Printf("%#v\n", getStatsResp)
+
+	versionResp, err := client.General.GetVersion()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%#v\n", versionResp)
+
+	//rand.Seed(time.Now().UnixNano())
+	list, _ := client.Inputs.GetInputList(&inputs.GetInputListParams{
+		InputKind: "as",
+	})
+
+	for _, _ = range list.Inputs {
+		if _, err := client.Inputs.SetInputVolume(&inputs.SetInputVolumeParams{
+			InputName:      "asd",
+			InputVolumeMul: rand.Float64(),
 		}); err != nil {
 			panic(err)
 		}
 	}
+	time.Sleep(30 * time.Second)
 
-	if len(list.Sources) == 0 {
-		fmt.Println("No sources!")
-		os.Exit(0)
-	}
+	//	if len(list.Inputs) == 0 {
+	//		fmt.Println("No sources!")
+	//		os.Exit(0)
+	//	}
 
 	fmt.Println("Test toggling the mute status of the first source...")
 
-	name := list.Sources[0].Name
-	resp, _ := client.Sources.GetVolume(&sources.GetVolumeParams{Source: name})
-	fmt.Printf("%s is muted? %t\n", name, resp.Muted)
-
-	_, _ = client.Sources.ToggleMute(&sources.ToggleMuteParams{Source: name})
-	resp, _ = client.Sources.GetVolume(&sources.GetVolumeParams{Source: name})
-	fmt.Printf("%s is muted? %t\n", name, resp.Muted)
+	//	name := list.Sources[0].Name
+	//	resp, _ := client.Sources.GetVolume(&sources.GetVolumeParams{Source: name})
+	//	fmt.Printf("%s is muted? %t\n", name, resp.Muted)
+	//
+	//	_, _ = client.Sources.ToggleMute(&sources.ToggleMuteParams{Source: name})
+	//	resp, _ = client.Sources.GetVolume(&sources.GetVolumeParams{Source: name})
+	//	fmt.Printf("%s is muted? %t\n", name, resp.Muted)
 }

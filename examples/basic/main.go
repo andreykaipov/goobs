941a5ab3f742891ccce4c8f664c19a1c0ae05af1
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2022-08-08 23:54:52 -0400

    examples

---
+++ examples/basic/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/andreykaipov/goobs"
+)
+
+func main() {
+	client, err := goobs.New("localhost:4444", goobs.WithPassword("goodpassword"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect()
+
+	version, _ := client.General.GetVersion()
+	fmt.Printf("OBS Studio version: %s\n", version.ObsVersion)
+	fmt.Printf("Websocket server version: %s\n", version.ObsWebSocketVersion)
+
+	fmt.Println("Scenes:")
+	resp, _ := client.Scenes.GetSceneList()
+	for _, v := range resp.Scenes {
+		fmt.Printf("%2d %s\n", v.SceneIndex, v.SceneName)
+	}
+}
--- examples/sources/main.go
+++ examples/sources/main.go
@@ -3,18 +3,36 @@ package main
 import (
 	"log"
 	"math/rand"
+	"os"
 	"time"
 
 	"github.com/andreykaipov/goobs"
+	"github.com/andreykaipov/goobs/api/events"
 	"github.com/andreykaipov/goobs/api/requests/inputs"
 )
 
-func blue(f string, v ...any)  { log.Printf("\033[34m"+f+"\033[0m", v...) }
 func green(f string, v ...any) { log.Printf("\033[32m"+f+"\033[0m", v...) }
+func blue(f string, v ...any)  { log.Printf("\033[34m"+f+"\033[0m", v...) }
 
 func main() {
-	client, _ := goobs.New("localhost:4444", goobs.WithPassword("goodpassword"))
+	client, err := goobs.New("localhost:4444", goobs.WithPassword("goodpassword"))
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer client.Disconnect()
+
+	go client.Listen(func(event any) {
+		switch e := event.(type) {
+		case *events.InputVolumeChanged:
+			green("%s's volume is now %f", e.InputName, e.InputVolumeDb)
+		case *events.ExitStarted:
+			green("Gracefully shutting down")
+			green("Bye")
+			os.Exit(0)
+		default:
+			blue("Unhandled: %#v", e)
+		}
+	})
 
 	rand.Seed(time.Now().UnixNano())
 
@@ -30,8 +48,9 @@ func main() {
 			blue("%s doesn't support audio", name)
 			continue
 		}
-
-		resp, _ := client.Inputs.GetInputVolume(&inputs.GetInputVolumeParams{InputName: name})
-		green("%s's volume is now %f", name, resp.InputVolumeDb)
 	}
+
+	// play around in OBS and maybe close it - observe the events it sends
+	//
+	time.Sleep(30 * time.Second)
 }

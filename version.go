10f327cfe98b577dbce5f1f4100422af4bc3471e
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2022-08-11 00:42:12 -0400

    v0.10.0 release for protocol v5.0.1
    This new protocol version does not change anything

--- version.go
+++ version.go
@@ -1,6 +1,6 @@
 package goobs
 
 const (
-	goobs_version                  = "0.9.0"
-	obs_websocket_protocol_version = "5.0.0"
+	goobs_version                  = "0.10.0"
+	obs_websocket_protocol_version = "5.0.1"
 )

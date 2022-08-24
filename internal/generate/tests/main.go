a9b8e00f89e2706a18fea934c445949a429621e0
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2022-08-24 00:30:48 -0400

    add a test when connecting to a nonrunning server

--- internal/generate/tests/main.go
+++ internal/generate/tests/main.go
@@ -114,9 +114,12 @@ func main() {
 }
 
 func generateClientTest() *Statement {
-	// tries to connect incorrectly
-	return Func().Id("Test_client").Params(Id("t").Op("*").Qual("testing", "T")).Block(
-		List(Id("_"), Id("err")).Op(":=").Qual(goobs, "New").Call(
+	s := Line()
+
+	s.Func().Id("Test_client").Params(Id("t").Op("*").Qual("testing", "T")).Block(
+		Var().Id("err").Error(),
+		// tries to connect incorrectly
+		List(Id("_"), Id("err")).Op("=").Qual(goobs, "New").Call(
 			Lit("localhost:").Op("+").Qual("os", "Getenv").Call(Lit("OBS_PORT")),
 			Qual(goobs, "WithPassword").Call(Lit("wrongpassword")),
 			Qual(goobs, "WithRequestHeader").Call(Qual("net/http", "Header").Values(Dict{
@@ -126,7 +129,20 @@ func generateClientTest() *Statement {
 		Qual(assert, "Error").Call(Id("t"), Id("err")),
 		Qual(assert, "IsType").Call(Id("t"), Op("&").Qual(websocket, "CloseError").Block(), Id("err")),
 		Qual(assert, "Equal").Call(Id("t"), Id("err").Assert(Op("*").Qual(websocket, "CloseError")).Dot("Code"), Lit(4009)),
+
+		// tries to connect to a nonrunning server
+		List(Id("_"), Id("err")).Op("=").Qual(goobs, "New").Call(
+			Lit("localhost:42069"),
+			Qual(goobs, "WithPassword").Call(Lit("wrongpassword")),
+			Qual(goobs, "WithRequestHeader").Call(Qual("net/http", "Header").Values(Dict{
+				Lit("User-Agent"): Index().String().Values(Lit("goobs-e2e/0.0.0")),
+			})),
+		),
+		Qual(assert, "Error").Call(Id("t"), Id("err")),
+		Qual(assert, "IsType").Call(Id("t"), Op("&").Qual("net", "OpError").Block(), Id("err")),
 	)
+
+	return s
 }
 
 func generateRequestTest(subclient, category string, structs map[string]StructFieldMap) *Statement {

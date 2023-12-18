a6aec2348c90a2286761fa33d9f82d6907febf2a
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2023-12-18 17:31:37 -0500

    ensure obs is running for readme snippet update

--- script/update-readme-snippets.sh
+++ script/update-readme-snippets.sh
@@ -22,8 +22,24 @@ replace_markdown() {
         mv "$f.tmp" "$f"
 }
 
+# we need to run the basic example against the dummy OBS instance
+ensure_obs() {
+        echo "Setting up a dummy OBS instance..."
+
+        obs="$(docker container inspect -f '{{.State.Status}}' obs || true)"
+
+        if [ "$obs" = running ]; then
+                echo "OBS container is already running"
+        else
+                docker run --rm --detach --name obs -p 4455:1234 ghcr.io/andreykaipov/goobs:latest
+                sleep 3
+        fi
+}
+
 main() {
         set -eu
+        ensure_obs
+
         content=$(cat _examples/basic/main.go)
         printf '```go\n%s\n```' "$content" | replace_markdown README.md snippet-1
 

845a618ef418f1dd1a526e207bc3038de3a45510
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2023-12-21 10:08:05 -0500

    use gh cli

--- script/bump-protocol-version.sh
+++ script/bump-protocol-version.sh
@@ -53,20 +53,13 @@ compare_versions() {
                 exit 1
         fi
 
-        if [ -n "$GITHUB_ACTIONS" ]; then
-                echo "bump=$bump" >>"$GITHUB_OUTPUT"
-                echo "next=$next" >>"$GITHUB_OUTPUT"
-        fi
+        echo "$bump" >/tmp/.goobs.protocol.bump
+        echo "$next" >/tmp/.goobs.protocol.next
 }
 
 bump_versions() {
         sed -i "s/$current/$next/g" version.go README.md
         make generate
-
-        if [ -n "$GITHUB_ACTIONS" ]; then
-                message="Bump OBS websocket protocol to $next"
-                echo "message=$message" >>"$GITHUB_OUTPUT"
-        fi
 }
 
 main() {

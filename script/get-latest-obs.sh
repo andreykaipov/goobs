cafc0a33ff24d7af5272a444ee9fc240722efbc3
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2024-06-10 17:12:30 -0400

    ci: fix docker image (#153)
    * fix docker image
    
    * lint
    
    * build image periodically (twice every month)
    
    i'm not sure why i removed weekly builds in the first place...
    
    * fix build?
    
    * aha
    
    ---------
    
    Co-authored-by: Andrey Kaipov <andreykaipov@users.noreply.github.com>

--- script/get-latest-obs.sh
+++ script/get-latest-obs.sh
@@ -6,28 +6,44 @@ main() {
         download_artifact >/tmp/obs.zip
         rm -rf /tmp/obs
         unzip -od /tmp/obs /tmp/obs.zip
+
+        # the names are different in their sha256 file
+        expected=$(cut -d' ' -f1 /tmp/obs/*.sha256 | awk '{print $1}')
         mv /tmp/obs/*.deb "$path"
+        echo "$expected $path" | sha256sum --check
         ls -al "$path"
 }
 
 download_artifact() {
         repo=obsproject/obs-studio
-        id=$(gh api /repos/$repo/actions/artifacts --jq '
-                [
+        artifacts_url=$(
+                gh api -XGET /repos/$repo/actions/runs \
+                        -fbranch=master \
+                        -fstatus=completed \
+                        -fexclude_pull_requests=true \
+                        -fevent=push \
+                        --jq '
+                                [
+                                        .workflow_runs[]
+                                        | select(.path==".github/workflows/push.yaml")
+                                ]
+                                | sort_by(.created_at)
+                                | reverse
+                                | first
+                                | .artifacts_url
+                        '
+        )
+        archive_download_url=$(
+                gh api "$artifacts_url" --jq '
                         .artifacts[]
-                        | select(
-                                .workflow_run.head_branch == "master" and
-                                (.name|contains("ubuntu")) and
-                                (.name|contains("dbgsym")|not)
-                        )
-                ]
-                | sort_by(.created_at)
-                | reverse
-                | first
-                | .id
-        ')
-        gh api "/repos/$repo/actions/artifacts/$id/zip"
+                        |= select(.name | test("ubuntu-2[4-9].[0-9]{2}-x86_64-[0-9a-f]+$"))
+                        | .artifacts
+                        | first
+                        | .archive_download_url
+                '
+        )
+        gh api "$archive_download_url"
 }
 
-set -eu
+set -eux
 main "$@"
